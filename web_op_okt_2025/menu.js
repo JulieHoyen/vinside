8f1ff2d2de3ddc09a9f9d360a09ec556c39df5b9
JulieHoyen <juho0001@stud.ek.dk>
2025-10-20 09:27:51 +0200

    Delete web_op_okt_2025 directory

--- web_op_okt_2025/menu.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('header nav');
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', () => {
-  const open = burger.classList.toggle('active');
-  nav.classList.toggle('active', open);
-  burger.setAttribute('aria-expanded', open);
-});
